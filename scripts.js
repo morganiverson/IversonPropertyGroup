874e22f79e3309b783f0155f45d76b8a95346698
mwiv <iversonmorgan@gmail.com>
2020-12-30 02:02:03 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,60 @@
+function calcMAO(DP, ARV, RC){
+    var IP = (ARV * .7) - RC; //INVERSOR PRICE
+
+    var MAO = IP - DP; //MAXIMUM ALLOWABLE OFFER
+    
+//    IP = IP + DP;
+    return {IP, MAO};
+}
+function fillTable(){
+    
+     var CELLKEYS = ["MIN2", "MIN15", "MIN1", "MIN5", "GIV", "PLU5", "PLU1", "PLU15", "PLU2"];
+    
+     var ARV = parseFloat(document.getElementById("ARV").value);
+    var RC = parseFloat(document.getElementById("RC").value);
+    var DP = parseFloat(document.getElementById("DP").value);
+    
+    //FILL DP, IP, MAO
+    CELLKEYS.forEach(function fill(item, index) {
+       console.log(item);
+        var offset = 0;
+        //SET OFFSET FROM GIVEN DP
+        switch(true) {
+            case item.indexOf("2") >= 0: offset = 2000; break;
+                case item.indexOf("15") >= 0: offset = 1500; break;
+                case item.indexOf("1") >= 0: offset = 1000; break;
+                case item.indexOf("5") >= 0: offset = 500; break;
+        }
+                
+        //MIN NEGATIVE PLUS POSITIVE
+        offset = (item.indexOf("MIN") >= 0) ? offset * -1 : offset;
+                
+        var DPOUT = document.getElementById(item + "DP");
+        var IPOUT = document.getElementById(item + "IP");
+        var MAOOUT = document.getElementById(item + "MAO");
+    
+        var output = calcMAO(DP + offset, ARV, RC);
+        console.log(output);
+        
+        document.getElementById(item + "DP").innerHTML = "$" + (DP + offset);
+        IPOUT.innerHTML = "$" + output["IP"];
+        MAOOUT.innerHTML = "$" + output["MAO"];
+        
+   });
+    
+   //FILL RC, ARV
+    var RCOUTS = document.getElementsByClassName("GIVRC");
+    
+    for(var i = 0; i < RCOUTS.length; i++) {
+        RCOUTS[i].innerHTML = "$" + RC;
+    };
+    
+    var ARVOUTS = document.getElementsByClassName("GIVARV");
+    
+    for(var i = 0; i < ARVOUTS.length; i++) {
+        ARVOUTS[i].innerHTML = "$" + ARV;
+    };
+    
+    
+    
+}
